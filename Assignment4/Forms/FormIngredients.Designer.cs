aa1b19c6fcf5c87bd5ce6dbb2e4d1e2c405b3492
Samuel Jeffman <sjeffman83@outlook.com>
2024-03-17 11:13:33 +0100

    Added necessary controls

--- Assignment4/Forms/FormIngredients.Designer.cs
+++ Assignment4/Forms/FormIngredients.Designer.cs
@@ -28,12 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormIngredients";
+            grpIngredient = new GroupBox();
+            btnOK = new Button();
+            btnCancel = new Button();
+            lblNumIngredients = new Label();
+            lblCurrNumber = new Label();
+            txtNameIngredient = new TextBox();
+            btnAdd = new Button();
+            btnEdit = new Button();
+            btnDelete = new Button();
+            lstIngredients = new ListBox();
+            grpIngredient.SuspendLayout();
+            SuspendLayout();
+            // 
+            // grpIngredient
+            // 
+            grpIngredient.Controls.Add(lstIngredients);
+            grpIngredient.Controls.Add(btnDelete);
+            grpIngredient.Controls.Add(btnEdit);
+            grpIngredient.Controls.Add(btnAdd);
+            grpIngredient.Controls.Add(txtNameIngredient);
+            grpIngredient.Location = new Point(12, 47);
+            grpIngredient.Name = "grpIngredient";
+            grpIngredient.Size = new Size(506, 351);
+            grpIngredient.TabIndex = 0;
+            grpIngredient.TabStop = false;
+            grpIngredient.Text = "Ingredient";
+            // 
+            // btnOK
+            // 
+            btnOK.Location = new Point(150, 418);
+            btnOK.Name = "btnOK";
+            btnOK.Size = new Size(75, 23);
+            btnOK.TabIndex = 1;
+            btnOK.Text = "OK";
+            btnOK.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(305, 418);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(75, 23);
+            btnCancel.TabIndex = 2;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // lblNumIngredients
+            // 
+            lblNumIngredients.AutoSize = true;
+            lblNumIngredients.Location = new Point(12, 24);
+            lblNumIngredients.Name = "lblNumIngredients";
+            lblNumIngredients.Size = new Size(127, 15);
+            lblNumIngredients.TabIndex = 3;
+            lblNumIngredients.Text = "Number of ingredients";
+            // 
+            // lblCurrNumber
+            // 
+            lblCurrNumber.AutoSize = true;
+            lblCurrNumber.Location = new Point(493, 24);
+            lblCurrNumber.Name = "lblCurrNumber";
+            lblCurrNumber.Size = new Size(25, 15);
+            lblCurrNumber.TabIndex = 4;
+            lblCurrNumber.Text = "000";
+            // 
+            // txtNameIngredient
+            // 
+            txtNameIngredient.Location = new Point(6, 22);
+            txtNameIngredient.Name = "txtNameIngredient";
+            txtNameIngredient.Size = new Size(391, 23);
+            txtNameIngredient.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(412, 22);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(75, 23);
+            btnAdd.TabIndex = 2;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(412, 64);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(75, 23);
+            btnEdit.TabIndex = 3;
+            btnEdit.Text = "Edit";
+            btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(412, 111);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(75, 23);
+            btnDelete.TabIndex = 4;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // lstIngredients
+            // 
+            lstIngredients.FormattingEnabled = true;
+            lstIngredients.ItemHeight = 15;
+            lstIngredients.Location = new Point(6, 64);
+            lstIngredients.Name = "lstIngredients";
+            lstIngredients.Size = new Size(391, 274);
+            lstIngredients.TabIndex = 5;
+            // 
+            // FormIngredients
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(530, 453);
+            Controls.Add(lblCurrNumber);
+            Controls.Add(lblNumIngredients);
+            Controls.Add(btnCancel);
+            Controls.Add(btnOK);
+            Controls.Add(grpIngredient);
+            Name = "FormIngredients";
+            Text = "FormIngredients";
+            grpIngredient.ResumeLayout(false);
+            grpIngredient.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private GroupBox grpIngredient;
+        private Button btnDelete;
+        private Button btnEdit;
+        private Button btnAdd;
+        private TextBox txtNameIngredient;
+        private Button btnOK;
+        private Button btnCancel;
+        private Label lblNumIngredients;
+        private Label lblCurrNumber;
+        private ListBox lstIngredients;
     }
 }
--- Assignment4/Forms/FormMain.Designer.cs
+++ Assignment4/Forms/FormMain.Designer.cs
@@ -28,12 +28,223 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            grpAddRecipe = new GroupBox();
+            lblNameRecipe = new Label();
+            textBox1 = new TextBox();
+            lblCategory = new Label();
+            cmbFoodCategory = new ComboBox();
+            btnAddIngredient = new Button();
+            btnAddRecipe = new Button();
+            lstRecipe = new ListBox();
+            btnEditStart = new Button();
+            btnEditFinish = new Button();
+            btnDelete = new Button();
+            btnClear = new Button();
+            lblInstructions = new Label();
+            lblListRecipe = new Label();
+            lblListCategory = new Label();
+            lblListNumIngredients = new Label();
+            txtDescription = new TextBox();
+            grpAddRecipe.SuspendLayout();
+            SuspendLayout();
+            // 
+            // grpAddRecipe
+            // 
+            grpAddRecipe.Controls.Add(txtDescription);
+            grpAddRecipe.Controls.Add(btnAddRecipe);
+            grpAddRecipe.Controls.Add(btnAddIngredient);
+            grpAddRecipe.Controls.Add(cmbFoodCategory);
+            grpAddRecipe.Controls.Add(lblCategory);
+            grpAddRecipe.Controls.Add(textBox1);
+            grpAddRecipe.Controls.Add(lblNameRecipe);
+            grpAddRecipe.Location = new Point(12, 12);
+            grpAddRecipe.Name = "grpAddRecipe";
+            grpAddRecipe.Size = new Size(365, 426);
+            grpAddRecipe.TabIndex = 0;
+            grpAddRecipe.TabStop = false;
+            grpAddRecipe.Text = "Add new recipe";
+            // 
+            // lblNameRecipe
+            // 
+            lblNameRecipe.AutoSize = true;
+            lblNameRecipe.Location = new Point(6, 32);
+            lblNameRecipe.Name = "lblNameRecipe";
+            lblNameRecipe.Size = new Size(88, 15);
+            lblNameRecipe.TabIndex = 0;
+            lblNameRecipe.Text = "Name of recipe";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(120, 29);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(239, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // lblCategory
+            // 
+            lblCategory.AutoSize = true;
+            lblCategory.Location = new Point(6, 65);
+            lblCategory.Name = "lblCategory";
+            lblCategory.Size = new Size(55, 15);
+            lblCategory.TabIndex = 2;
+            lblCategory.Text = "Category";
+            // 
+            // cmbFoodCategory
+            // 
+            cmbFoodCategory.FormattingEnabled = true;
+            cmbFoodCategory.Location = new Point(65, 62);
+            cmbFoodCategory.Name = "cmbFoodCategory";
+            cmbFoodCategory.Size = new Size(134, 23);
+            cmbFoodCategory.TabIndex = 3;
+            // 
+            // btnAddIngredient
+            // 
+            btnAddIngredient.Location = new Point(205, 62);
+            btnAddIngredient.Name = "btnAddIngredient";
+            btnAddIngredient.Size = new Size(154, 23);
+            btnAddIngredient.TabIndex = 4;
+            btnAddIngredient.Text = "Add Ingredients";
+            btnAddIngredient.UseVisualStyleBackColor = true;
+            // 
+            // btnAddRecipe
+            // 
+            btnAddRecipe.Location = new Point(6, 397);
+            btnAddRecipe.Name = "btnAddRecipe";
+            btnAddRecipe.Size = new Size(353, 23);
+            btnAddRecipe.TabIndex = 6;
+            btnAddRecipe.Text = "Add Recipe";
+            btnAddRecipe.UseVisualStyleBackColor = true;
+            // 
+            // lstRecipe
+            // 
+            lstRecipe.FormattingEnabled = true;
+            lstRecipe.ItemHeight = 15;
+            lstRecipe.Location = new Point(383, 44);
+            lstRecipe.Name = "lstRecipe";
+            lstRecipe.Size = new Size(671, 334);
+            lstRecipe.TabIndex = 1;
+            // 
+            // btnEditStart
+            // 
+            btnEditStart.Location = new Point(383, 384);
+            btnEditStart.Name = "btnEditStart";
+            btnEditStart.Size = new Size(75, 23);
+            btnEditStart.TabIndex = 2;
+            btnEditStart.Text = "Edit-Begin";
+            btnEditStart.UseVisualStyleBackColor = true;
+            // 
+            // btnEditFinish
+            // 
+            btnEditFinish.Location = new Point(560, 384);
+            btnEditFinish.Name = "btnEditFinish";
+            btnEditFinish.Size = new Size(75, 23);
+            btnEditFinish.TabIndex = 3;
+            btnEditFinish.Text = "Edit-Finish";
+            btnEditFinish.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(754, 384);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(75, 23);
+            btnDelete.TabIndex = 4;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(928, 384);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(126, 23);
+            btnClear.TabIndex = 5;
+            btnClear.Text = "Clear Selection";
+            btnClear.UseVisualStyleBackColor = true;
+            // 
+            // lblInstructions
+            // 
+            lblInstructions.AutoSize = true;
+            lblInstructions.Location = new Point(607, 417);
+            lblInstructions.Name = "lblInstructions";
+            lblInstructions.Size = new Size(198, 15);
+            lblInstructions.TabIndex = 6;
+            lblInstructions.Text = "Double click an item for instructions";
+            // 
+            // lblListRecipe
+            // 
+            lblListRecipe.AutoSize = true;
+            lblListRecipe.Location = new Point(420, 12);
+            lblListRecipe.Name = "lblListRecipe";
+            lblListRecipe.Size = new Size(39, 15);
+            lblListRecipe.TabIndex = 7;
+            lblListRecipe.Text = "Name";
+            // 
+            // lblListCategory
+            // 
+            lblListCategory.AutoSize = true;
+            lblListCategory.Location = new Point(697, 12);
+            lblListCategory.Name = "lblListCategory";
+            lblListCategory.Size = new Size(55, 15);
+            lblListCategory.TabIndex = 8;
+            lblListCategory.Text = "Category";
+            // 
+            // lblListNumIngredients
+            // 
+            lblListNumIngredients.Location = new Point(974, 9);
+            lblListNumIngredients.Name = "lblListNumIngredients";
+            lblListNumIngredients.Size = new Size(80, 32);
+            lblListNumIngredients.TabIndex = 9;
+            lblListNumIngredients.Text = "Number of ingredients";
+            lblListNumIngredients.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // txtDescription
+            // 
+            txtDescription.Location = new Point(6, 91);
+            txtDescription.Multiline = true;
+            txtDescription.Name = "txtDescription";
+            txtDescription.Size = new Size(353, 300);
+            txtDescription.TabIndex = 7;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1066, 450);
+            Controls.Add(lblListNumIngredients);
+            Controls.Add(lblListCategory);
+            Controls.Add(lblListRecipe);
+            Controls.Add(lblInstructions);
+            Controls.Add(btnClear);
+            Controls.Add(btnDelete);
+            Controls.Add(btnEditFinish);
+            Controls.Add(btnEditStart);
+            Controls.Add(lstRecipe);
+            Controls.Add(grpAddRecipe);
+            Name = "FormMain";
+            Text = "Recipe Book by Samuel Jeffman";
+            grpAddRecipe.ResumeLayout(false);
+            grpAddRecipe.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private GroupBox grpAddRecipe;
+        private Button btnAddIngredient;
+        private ComboBox cmbFoodCategory;
+        private Label lblCategory;
+        private TextBox textBox1;
+        private Label lblNameRecipe;
+        private Button btnAddRecipe;
+        private ListBox lstRecipe;
+        private Button btnEditStart;
+        private Button btnEditFinish;
+        private Button btnDelete;
+        private Button btnClear;
+        private Label lblInstructions;
+        private Label lblListRecipe;
+        private Label lblListCategory;
+        private Label lblListNumIngredients;
+        private TextBox txtDescription;
     }
 }
