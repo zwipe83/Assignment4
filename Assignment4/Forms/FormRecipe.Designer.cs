77017ee8dc3b66858fd7646cc97aa743ba309c2c
Samuel Jeffman <sjeffman83@outlook.com>
2024-03-25 20:43:40 +0100

    New form

---
+++ Assignment4/Forms/FormRecipe.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Assignment4.Forms
+{
+    partial class FormRecipe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblIngredients = new Label();
+            lblInstructions = new Label();
+            SuspendLayout();
+            // 
+            // lblIngredients
+            // 
+            lblIngredients.AutoSize = true;
+            lblIngredients.Location = new Point(12, 9);
+            lblIngredients.Name = "lblIngredients";
+            lblIngredients.Size = new Size(78, 15);
+            lblIngredients.TabIndex = 0;
+            lblIngredients.Text = "INGREDIENTS";
+            // 
+            // lblInstructions
+            // 
+            lblInstructions.AutoSize = true;
+            lblInstructions.Location = new Point(12, 71);
+            lblInstructions.Name = "lblInstructions";
+            lblInstructions.Size = new Size(87, 15);
+            lblInstructions.TabIndex = 0;
+            lblInstructions.Text = "INSTRUCTIONS";
+            // 
+            // FormRecipe
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblInstructions);
+            Controls.Add(lblIngredients);
+            Name = "FormRecipe";
+            Text = "FormRecipe";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblIngredients;
+        private Label lblInstructions;
+    }
+}
---
+++ Assignment4/Forms/FormRecipe.cs
@@ -0,0 +1,57 @@
+﻿namespace Assignment4.Forms
+{
+    public partial class FormRecipe : Form
+    {
+        #region Fields
+        private Recipe _recipe;
+        #endregion
+        #region Properties
+        public Recipe Recipe
+        {
+            get => _recipe;
+            set => _recipe = value;
+        }
+        #endregion
+        #region Constructors
+        public FormRecipe(Recipe recipe)
+        {
+            InitializeComponent();
+
+            Recipe = recipe;
+
+            InitGUI();
+        }
+        #endregion
+        #region Initializers
+        private void InitGUI()
+        {
+            this.Text = Recipe.Name;
+            lblIngredients.Text = $"INGREDIENTS\n{GetIngredients(Recipe)}";
+            lblInstructions.Text = $"\n\nINSTRUCTIONS\n{GetInstructions(Recipe)}";
+        }
+        #endregion
+        #region Methods
+        private string GetIngredients(Recipe recipe)
+        {
+            string? ingredients = String.Empty;
+            foreach (var ingredient in recipe.Ingredients)
+            {
+                ingredients += $"{ingredient},";
+            }
+            ingredients.Remove(ingredients.Length - 1); //Remove last ','
+
+            return ingredients;
+        }
+        private string GetInstructions(Recipe recipe)
+        {
+            string? instructions = String.Empty;
+            foreach (var instruction in recipe.Description)
+            {
+                instructions += $"{instruction}";
+            }
+
+            return instructions;
+        }
+        #endregion
+    }
+}
